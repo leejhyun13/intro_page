e4fe8044d5304ec98e3d8840dc67b49b46a9e2f3
khakisage <jongjin201@gmail.com>
2025-04-25 15:41:03 +0900

    feat: 'About Us' 섹션 내용 업데이트 및 반응형 스타일 개선 feat: 다운로드 섹션에 모바일 전용 이미지 추가 및 텍스트 크기 조정 fix: 푸터에 경계선 추가 및 SNS 제목 제거 feat: 헤더에 모바일 전용 로고 추가 및 네비게이션 메뉴 스타일 개선 feat: 서비스 캐러셀 내용 업데이트 및 반응형 디자인 개선

--- src/components/ServiceCarousel.jsx
+++ src/components/ServiceCarousel.jsx
@@ -7,47 +7,60 @@ const ServiceCarousel = () => {
       <Swiper spaceBetween={50} slidesPerView={1} loop={true}>
         <SwiperSlide>
           <div className="flex flex-col justify-between h-screen w-screen">
-            <div className="bg-white p-10 text-center flex flex-col justify-end items-center h-full">
-              <h3 className="text-7xl font-semibold">
+            <div className="bg-white p-10 text-center flex flex-col justify-center items-center h-full">
+              <h3 className="text-7xl font-semibold max-[431px]:text-2xl">
                 더 많은 연결, 더 높은 매칭
               </h3>
-              <p className="mt-16 text-gray-600 text-3xl">
-                버려지는 물건에도 누군가의 취향이 담겨 있습니다.
-              </p>
-              <p className="text-gray-600 text-3xl mt-2">
-                바나나창고는 ‘더 이상 필요 없지만 누군가에겐 꼭 필요한 것들'을
-              </p>
-              <p className="text-gray-600 text-3xl mt-2">
-                연결하는 신개념 물물교환 플랫폼입니다.
-              </p>
+              <div className="flex flex-col items-start">
+                <p className="mt-16 text-gray-600 text-3xl max-[431px]:text-sm">
+                  물물교환은 상호간의 필요가 충족되어야 합니다.
+                </p>
+                <p className="text-gray-600 text-3xl mt-2 max-[431px]:text-sm">
+                  저희는 1 : N 의 교환 방식을 도입하여
+                </p>
+                <p className="text-gray-600 text-3xl mt-2 max-[431px]:text-sm">
+                  그 물건이 필요한 사람들로부터 제안을 받아
+                </p>
+                <p className="text-gray-600 text-3xl mt-2 max-[431px]:text-sm">
+                  교환 기회를 넓힙니다.
+                </p>
+              </div>
             </div>
-            <div className="flex justify-center">
+            <div className="flex justify-center max-[431px]:overflow-hidden max-[431px]:h-[50%]">
               <img
                 src="/service_1.png"
                 alt="Service 1"
-                className="w-2/3 object-contain"
+                className="w-2/3 object-contain max-[431px]:w-full max-[431px]:object-cover"
               />
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="flex flex-col justify-between h-screen w-screen">
-            <div className="bg-white p-10 text-center flex flex-col justify-center items-center h-full">
-              <h3 className="text-7xl font-semibold">
+            <div className="bg-white p-10 text-center flex flex-col justify-center items-center h-full ">
+              <h3 className="text-7xl font-semibold max-[431px]:text-2xl">
                 환경을 위한 지속 가능한 소비
               </h3>
-              <p className="mt-16 text-gray-600 text-3xl">
-                버리기엔 아까운 물건이, 누군가에겐 필요한 물건일 수 있습니다.
-              </p>
-              <p className="text-gray-600 text-3xl mt-2">
-                교환을 통해 버리지 않고, 지속가능한 소비 문화를 만듭니다.
-              </p>
+              <div className="flex flex-col items-start">
+                <p className="mt-16 text-gray-600 text-3xl max-[431px]:text-sm">
+                  버리기엔 아까운 물건이,
+                </p>
+                <p className="text-gray-600 text-3xl mt-2 max-[431px]:text-sm">
+                  누군가에게는 꼭 필요한 물건이 될 수 있습니다.
+                </p>
+                <p className="text-gray-600 text-3xl mt-2 max-[431px]:text-sm">
+                  교환을 통해 버리지 않고,
+                </p>
+                <p className="text-gray-600 text-3xl mt-2 max-[431px]:text-sm">
+                  물건을 재사용하여 환경을 지켜주세요.
+                </p>
+              </div>
             </div>
-            <div className="flex justify-center">
+            <div className="flex justify-center max-[431px]:overflow-hidden max-[431px]:h-[50%]">
               <img
                 src="/service_2.png"
                 alt="Service 1"
-                className="w-2/4 object-contain"
+                className="w-2/4 object-contain max-[431px]:w-full max-[431px]:object-cover"
               />
             </div>
           </div>
